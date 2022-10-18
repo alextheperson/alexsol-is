623798649a4e4496b3b3f65115dacb3fa29202b3
Alex <alexanderesolis@gmail.com>
2022-10-18 14:46:14 -0400

    maybe I don't need to do that

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
 
 app.use(express.static("public/static"))
 
 const PORT = process.env.PORT || 8000;
-
-var con = mysql.createConnection({
-    host: "localhost:5522",
-    user: "alexizys_pojects",
-    password: "aF6jfsUrZinun@4"
-});
 
 con.connect(function (err) {
     if (err) throw err;
@@ -19,7 +12,7 @@ con.connect(function (err) {
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/home.html")
-    res.send('Hello world');
+    // res.send('Hello world');
 });
 
 app.get('/about', (req, res) => {
@@ -36,12 +29,5 @@ app.get('/projects/*', (req, res) => {
     res.sendFile(__dirname + "/public/projects.html")
     // res.send('Hello world');
 });
-
-app.get("/test", (req, res) => {
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        res.send(`<p>Result: ${result}</p>`)
-    });
-})
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
