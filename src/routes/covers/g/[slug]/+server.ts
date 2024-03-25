0f7ffac1dd1f358b33a39ae943fb359e269837c6
Alex <alexanderesolis@gmail.com>
2024-03-25 11:05:01 -0400

    added the ability to cache the covers for 1 day

--- src/routes/covers/g/[slug]/+server.ts
+++ src/routes/covers/g/[slug]/+server.ts
@@ -1,13 +1,10 @@
-import config from '$lib/config';
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
+import { getGuideResource } from '$lib/getGuides.js';
 
 export function GET({ params }) {
-	const guideDirectory = fileURLToPath(
-		new URL('../../../../..' + config.GUIDES_PATH, import.meta.url)
-	);
-	const file = readFileSync(join(guideDirectory, params.slug, 'cover.png'));
+	const file = getGuideResource(params.slug, 'cover.png');
+	const cacheAge = 24 * 60 * 60; // one day
 
-	return new Response(file, { headers: { 'Content-Type': 'image/png' } });
+	return new Response(file, {
+		headers: { 'Content-Type': 'image/png', 'cache-control': `max-age=${cacheAge}` }
+	});
 }
--- src/routes/covers/p/[slug]/+server.ts
+++ src/routes/covers/p/[slug]/+server.ts
@@ -2,6 +2,9 @@ import { getProjectResource } from '$lib/getProjects.js';
 
 export function GET({ params }) {
 	const file = getProjectResource(params.slug, 'cover.png');
+	const cacheAge = 24 * 60 * 60; // one day
 
-	return new Response(file, { headers: { 'Content-Type': 'image/png' } });
+	return new Response(file, {
+		headers: { 'Content-Type': 'image/png', 'cache-control': `max-age=${cacheAge}` }
+	});
 }
