19f76f4f6566f8bc583e24953d77abcf8a4028f2
Alex Solis <git@alexsol.is>
2026-01-15 11:16:12 -0500

    Add mobile responsivness with a toggle-able dropdown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,12 +22,23 @@
 <header>
 	<div class="main-segment">
 		<a class="vanilla" href="/"><h1>Alexsol.is</h1></a>
-		<a class={page.url.pathname === '/' ? 'selected' : ''} href="/">Home</a>
-		<a class={page.url.pathname.endsWith('about') ? 'selected' : ''} href="/about">About</a>
-		<a class={page.url.pathname.endsWith('projects') ? 'selected' : ''} href="/projects">Projects</a
-		>
-		<!--<a class={$page.url.pathname.endsWith('guides') ? 'selected' : ''} href="/guides">Guides</a>-->
-		<a class={page.url.pathname.endsWith('theater') ? 'selected' : ''} href="/theater">Theater</a>
+		<label class="menu-opener">
+			<input type="checkbox" class="menu" aria-label="Show Menu" checked={false} />
+			<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" stroke="white" stroke-width="5">
+				<line x1="5" y1="5" x2="95" y2="5"></line>
+				<line x1="5" y1="50" x2="95" y2="50"></line>
+				<line x1="5" y1="95" x2="95" y2="95"></line>
+			</svg>
+		</label>
+
+		<div class="dropdown">
+			<a class={page.url.pathname === '/' ? 'selected' : ''} href="/">Home</a>
+			<a class={page.url.pathname.endsWith('about') ? 'selected' : ''} href="/about">About</a>
+			<a class={page.url.pathname.endsWith('projects') ? 'selected' : ''} href="/projects"
+				>Projects</a
+			>
+			<a class={page.url.pathname.endsWith('theater') ? 'selected' : ''} href="/theater">Theater</a>
+		</div>
 	</div>
 	<div class="shadow-segment"></div>
 	{#key shownFact}
@@ -51,6 +62,8 @@
 		display: flex;
 		--slant-size: 50px;
 		margin-bottom: 20px;
+		z-index: 1;
+		position: relative;
 	}
 
 	h1 {
@@ -67,10 +80,8 @@
 		justify-content: center;
 		align-items: center;
 		flex-grow: 0;
-		transition: 2s;
 
 		position: relative;
-		z-index: 9;
 	}
 
 	.shadow-segment {
@@ -87,10 +98,8 @@
 		justify-content: center;
 		align-items: center;
 		flex-grow: 0;
-		transition: 2s;
 
 		position: relative;
-		z-index: 9;
 		translate: calc(-0.5 * var(--slant-size)) 0;
 	}
 
@@ -160,6 +169,29 @@
 		border-inline-start: 0px none transparent;
 	}
 
+	.menu-opener input {
+		width: 0;
+		height: 0;
+		position: fixed;
+		top: 0;
+		outline: none;
+	}
+
+	.menu-opener {
+		display: none;
+		height: 100%;
+		aspect-ratio: 1 / 1;
+	}
+
+	div.dropdown {
+		display: flex;
+		flex-direction: row;
+	}
+
+	div.dropdown * {
+		z-index: 10;
+	}
+
 	@media only screen and (max-width: 1100px) {
 		.main-segment {
 			flex-grow: 1;
@@ -172,9 +204,69 @@
 		}
 	}
 
+	@media (max-width: 768px) {
+		main::before,
+		main::after {
+			content: unset;
+		}
+
+		main {
+			margin: 0;
+			width: 100%;
+			font-size: 0.9em;
+		}
+
+		.main-segment {
+			padding: 20px;
+			overflow: visible;
+			clip-path: unset;
+			position: relative;
+		}
+
+		.menu-opener {
+			display: block;
+		}
+
+		.menu-opener:focus-within svg {
+			outline: auto;
+		}
+
+		div.dropdown {
+			display: flex !important;
+			flex-direction: column;
+			position: absolute;
+			right: 0;
+			bottom: 0;
+			translate: 0 var(--slant-size);
+			padding: 20px 20px calc(20px + var(--slant-size)) 20px;
+			background-color: var(--secondary);
+			clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - var(--slant-size)));
+			transition: 0.2s;
+			gap: 1em;
+			text-align: center;
+			font-size: 1.2em;
+
+			z-index: -1;
+		}
+
+		.menu-opener:has(input:checked) + div.dropdown {
+			translate: 0 100%;
+		}
+
+		a::before,
+		a::after {
+			content: unset;
+			display: none;
+		}
+	}
+
 	@media (prefers-reduced-motion: reduce) {
 		a:not(.vanilla)::after,
 		a:not(.vanilla)::before {
+			transition: 0s;
+		}
+
+		div.dropdown {
 			transition: 0s;
 		}
 	}
